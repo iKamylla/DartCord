5d4f73640c7d8e13c4175d7f54086cb800005048
Kamylla Rodrigues da Silva <07263736217@sefin.ro.gov.br>
2026-09-16 11:47:22 -0400

    Ajuste no main.dart

--- main.dart
+++ main.dart
@@ -4,49 +4,44 @@ import 'models/mensagem.dart';
 import 'models/canal.dart';
 
 void main() {
-    final usuario = Usuario(
-        nome: 'Pedro',
-        apelido: 'PG',
-        online: true,
-    );
+  final usuario = Usuario(nome: 'Pedro', apelido: 'PG', online: true);
 
-    final moderador = Moderador(
-        nome: 'Kamylla',
-        apelido: 'Kamy',
-        online: true,
-    );
+  final moderador = Moderador(nome: 'Kamylla', apelido: 'Kamy', online: true);
 
-    usuario.advertir();
+  usuario.advertir();
 
-    final List<Usuario> usuarios = [usuario, moderador];
+  final List<Usuario> usuarios = [usuario, moderador];
 
-    final canais = ['geral', 'dart', 'flutter'];
+  final canais = ['geral', 'dart', 'flutter'];
 
-    final canalDart = Canal(nome: 'dart');
+  final canalDart = Canal(nome: 'dart');
 
-    final msg1 = Mensagem(autor: usuario, texto: 'Olá, DartCord!');
-    final msg2 = Mensagem(autor: moderador, texto: 'Pratiquem! Pratiquem!');
+  final msg1 = Mensagem(autor: usuario, texto: 'Olá, DartCord!');
+  final msg2 = Mensagem(
+    autor: moderador,
+    texto: 'Continuem praticando. Pratiquem!',
+  );
 
-    canalDart.addMensagem(msg1);
-    canalDart.addMensagem(msg2);
+  canalDart.addMensagem(msg1);
+  canalDart.addMensagem(msg2);
 
-    print('DARTCORD\nServidor: Programação Mobile');
+  print('DARTCORD\nServidor: Programação Mobile');
 
-    print('\nUsuários online:');
-    for (final user in usuarios) {
-        if (user.online) {
-            print(user.apelido);
-        }
+  print('\nUsuários online:');
+  for (final user in usuarios) {
+    if (user.online) {
+      print(user.apelido);
     }
+  }
 
-    print('\nCanais:');
-    for (final canal in canais) {
-        print('#$canal');
-    }
+  print('\nCanais:');
+  for (final canal in canais) {
+    print('#$canal');
+  }
 
-    print('\nExibindo mensagens do canal:');
-    print('#${canalDart.nome}');
-    for (final msg in canalDart.mensagens) {
-        print('${msg.autor.apelido}: ${msg.texto}');
-    }
+  print('\nExibindo mensagens do canal:');
+  print('#${canalDart.nome}');
+  for (final msg in canalDart.mensagens) {
+    print('${msg.autor.apelido}: ${msg.texto}');
+  }
 }
